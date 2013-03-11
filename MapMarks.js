3ee9fb420033f7768188f4a89427c0d84026233b
gael <gael@ubuntu.ubuntu-domain>
2013-03-11 14:42:36 -0400

    Science tags hardcoded

--- MapMarks.js
+++ MapMarks.js
@@ -38,12 +38,12 @@ if (Meteor.isClient) {
 	};	
 	Template.pages.NewsTagged = function() {
 		return Pages.find({"tags" : ["News"]});
-	};	
-	Template.pages.CookingTagged = function() {
-		return Pages.find({x: {$lt: 400}}, {sort: {score: -1}});
-	};	
+	};		
 	Template.pages.ScienceTagged = function() {
-		return Pages.find({x: {$lt: 400}}, {sort: {score: -1}});
+		return Pages.find({"tags" : ["Science"]});
+	};			
+	Template.pages.CookingTagged = function() {
+		return Pages.find({"tags" : ["Cooking"]});
 	};
 	
 	Meteor.startup(function () {
